64a904e2f6dc1378cda75584c99dcd2b1df6e775
robinbenitezmora <robinbenitez1990@gmail.com>
2023-07-21 20:23:54 -0500

    Create 'ControlLogic.java' class and other ones

---
+++ userInterface/logic/GameLogic.java
@@ -0,0 +1,16 @@
+package userInterface.logic;
+
+import constants.GameState;
+import domain.SudokuGame;
+
+public class GameLogic {
+
+ public static GameState checkForCompletion(int[][] newGridState) {
+  return null;
+ }
+
+ public static SudokuGame getNewGame() {
+  return null;
+ }
+
+}
